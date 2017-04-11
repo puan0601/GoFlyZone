f4435bb0a3a98d086a5c354f788287dc2ffd6469
puan0601 <anton.pugach@gmail.com>
2017-04-11 14:48:38 -0700

    add new place button and seed file with place locations

--- server/config/seed.js
+++ server/config/seed.js
@@ -13,32 +13,15 @@ export default function seedDatabaseIfNeeded() {
     Thing.find({}).remove()
       .then(() => {
         Thing.create({
-          name: 'Development Tools',
-          info: 'Integration with popular tools such as Webpack, Gulp, Babel, TypeScript, Karma, '
-                + 'Mocha, ESLint, Node Inspector, Livereload, Protractor, Pug, '
-                + 'Stylus, Sass, and Less.'
+          name: 'Treasure Island',
+          info: 'Great flying, views overlook the city and bay, kind of windy.'
         }, {
-          name: 'Server and Client integration',
-          info: 'Built with a powerful and fun stack: MongoDB, Express, '
-                + 'AngularJS, and Node.'
+          name: 'Berkeley Marina',
+          info: 'Right on the water, ample open space'
+
         }, {
-          name: 'Smart Build System',
-          info: 'Build system ignores `spec` files, allowing you to keep '
-                + 'tests alongside code. Automatic injection of scripts and '
-                + 'styles into your index.html'
-        }, {
-          name: 'Modular Structure',
-          info: 'Best practice client and server structures allow for more '
-                + 'code reusability and maximum scalability'
-        }, {
-          name: 'Optimized Build',
-          info: 'Build process packs up your templates as a single JavaScript '
-                + 'payload, minifies your scripts/css/images, and rewrites asset '
-                + 'names for caching.'
-        }, {
-          name: 'Deployment Ready',
-          info: 'Easily deploy your app to Heroku or Openshift with the heroku '
-                + 'and openshift subgenerators'
+          name: 'Dog Patch',
+          info: 'Large open space, on the water, not many obstacles.'
         });
       })
     .then(() => console.log('finished populating things'))
