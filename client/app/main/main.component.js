1d9991f7e735624fb84cdbb6985a67913b8d4b77
puan0601 <anton.pugach@gmail.com>
2017-04-10 21:58:45 -0700

    places are displaying but im too tired to format, tomorrow

--- client/app/main/main.component.js
+++ client/app/main/main.component.js
@@ -3,40 +3,28 @@ const ngRoute = require('angular-route');
 import routing from './main.routes';
 
 export class MainController {
-
-  awesomeThings = [];
+  isCollapsed = true;
+  places = [];
   newThing = '';
+  myMap = '';
   /*@ngInject*/
   constructor($http, NgMap) {
     this.$http = $http;
-    this.NgMap = NgMap;
+    this.myMap = NgMap.getMap().then(function(map) {
+      map.setCenter({lat: 37.7909, lng: -122.4013});
+      map.setZoom(12);
+      map.data.loadGeoJson('https://raw.githubusercontent.com/mapbox/drone-feedback/master/sources/geojson/5_mile_airport.geojson');
+      map.data.loadGeoJson('https://raw.githubusercontent.com/mapbox/drone-feedback/master/sources/geojson/us_military.geojson');
+      map.data.loadGeoJson('https://raw.githubusercontent.com/mapbox/drone-feedback/master/sources/geojson/us_national_park.geojson');
+    });
   }
 
   $onInit() {
     this.$http.get('/api/things')
       .then(response => {
-        this.awesomeThings = response.data;
+        this.places = response.data;
       });
-    this.NgMap.getMap().then(function(map) {
-      map.setCenter({lat: 37.7909, lng: -122.4013});
-      map.data.loadGeoJson('https://raw.githubusercontent.com/mapbox/drone-feedback/master/sources/geojson/5_mile_airport.geojson');
-      map.data.loadGeoJson('https://raw.githubusercontent.com/mapbox/drone-feedback/master/sources/geojson/us_military.geojson');
-      map.data.loadGeoJson('https://raw.githubusercontent.com/mapbox/drone-feedback/master/sources/geojson/us_national_park.geojson');
-
-      console.log(map.getCenter);
-      console.log(map.getCenter());
-      console.log('markers', map.markers);
-      console.log('shapes', map.shapes);
-    });
   }
-
-  // NgMap() {
-  //   NgMap.getMap().then(function(map) {
-  //     console.log(map.getCenter());
-  //     console.log('markers', map.markers);
-  //     console.log('shapes', map.shapes);
-  //   });
-  // }
 
   addThing() {
     if(this.newThing) {
--- client/app/main/main.component.spec.js
+++ client/app/main/main.component.spec.js
@@ -28,7 +28,7 @@ describe('Component: MainComponent', function() {
   it('should attach a list of things to the controller', function() {
     mainComponent.$onInit();
     $httpBackend.flush();
-    expect(mainComponent.awesomeThings.length)
+    expect(mainComponent.places.length)
       .to.equal(4);
   });
 });
